87a33c9c7b1b5ae0a251b796b2045f27e59f78d6
Aashish012-rgb <182605557+Aashish012-rgb@users.noreply.github.com>
2026-03-03 15:46:50 +0545

    Added the location system using the leaflet and openstreet map

--- HomeRental/home/models.py
+++ HomeRental/home/models.py
@@ -24,6 +24,8 @@ class Property(models.Model):
     property_type = models.CharField(max_length=10, choices=PROPERTY_TYPE)
     image = models.ImageField(upload_to='property_images/')
     created_at = models.DateTimeField(auto_now_add=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.title
