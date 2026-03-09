5f5fbbbf10cd3ea36670c7b84384cb66fd27c90e
Aashish012-rgb <182605557+Aashish012-rgb@users.noreply.github.com>
2026-03-09 20:55:14 +0545

    added the progress of today

--- HomeRental/wsgi.py
+++ HomeRental/wsgi.py
@@ -1,9 +1,12 @@
 """
-WSGI config for HomeRental project.
+WSGI Configuration for HomeRental Project
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+Exposes the WSGI callable as a module-level variable named 'application'.
+This is used by production web servers (Gunicorn, uWSGI, etc.) to run the Django application.
 
-For more information on this file, see
+WSGI (Web Server Gateway Interface) is the Python standard for web applications.
+
+For more information, see:
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
@@ -11,6 +14,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# Set the Django settings module to use
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HomeRental.settings')
 
+# Get the WSGI application - this is what web servers call to handle requests
 application = get_wsgi_application()
--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,13 @@
-from django.contrib import admin
-from .models import home
-# Register your models here.
+"""
+Django Admin Configuration
+Registers models with Django admin panel for easy management.
+"""
 
-admin.site.register(home)
+from django.contrib import admin
+from .models import home, Testimonial
+
+# Register your models here.
+# These models will appear in the Django admin panel for database management
+
+admin.site.register(home)  # Register legacy home model for admin management
+admin.site.register(Testimonial)  # Register testimonials for admin moderation
